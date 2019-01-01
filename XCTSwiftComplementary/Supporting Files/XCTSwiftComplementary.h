6a8b4cdba00228ae9314b119255bc847c5d47dc5
WeZZard <me@wezzard.com>
2019-01-02 04:02:51 +0800

    Renames the project.

---
+++ XCTSwiftComplementary/Supporting Files/XCTSwiftComplementary.h
@@ -0,0 +1,8 @@
+//
+//  XCTSwiftComplementary.h
+//  XCTSwiftComplementary
+//
+//  Created on 17/10/2018.
+//
+
+#import <XCTSwiftComplementary/XCTestCase+AssertingThrows.h>
--- XCTestSophie/Supporting Files/XCTestSophie.h
+++
@@ -1,8 +0,0 @@
-//
-//  XCTestSophie.h
-//  XCTestSophie
-//
-//  Created on 17/10/2018.
-//
-
-#import <XCTestSophie/XCTestCase+AssertingThrows.h>

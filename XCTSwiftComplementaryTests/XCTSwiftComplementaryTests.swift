8fbe3b4e22b77d07a3f88d8d6d5b74a443dd39a0
WeZZard <me@wezzard.com>
2019-01-02 04:14:37 +0800

    Adjusts test targets.

---
+++ XCTSwiftComplementaryTests/XCTSwiftComplementaryTests.swift
@@ -0,0 +1,16 @@
+//
+//  XCTSwiftComplementaryTests.swift
+//  XCTSwiftComplementary
+//
+//  Created on 2/1/2019.
+//
+
+import XCTest
+
+@testable
+import XCTSwiftComplementary
+
+class XCTSwiftComplementaryTests: XCTestCase {
+    // Currently no test cases here. We need to keep a file in the test
+    // target to keep the CI process conssitent.
+}
